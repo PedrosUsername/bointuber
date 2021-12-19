d14c9998e7bbe0774dd796695ea3d54ed71efcc8
rogs <pilique11@gmail.com>
2021-12-19 14:37:18 -0300

    add sana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IchikaComponent } from './ichika/ichika.component';
+import { SanaComponent } from './sana/sana.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IchikaComponent
+    IchikaComponent,
+    SanaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sana/sana.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SanaComponent } from './sana.component';
+
+describe('SanaComponent', () => {
+  let component: SanaComponent;
+  let fixture: ComponentFixture<SanaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SanaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SanaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
