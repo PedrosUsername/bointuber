7ad424d40f2bbfe4f0f294060b15238606e0ec01
rogs <pilique11@gmail.com>
2022-01-01 12:32:36 -0300

    update about

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BridgeService } from '../shared/bridge.service';
 
 @Component({
   selector: 'app-about-page',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private bridge: BridgeService
+  ){}
 
   ngOnInit(): void {
+  }
+
+  onClick(message: string){
+    this.bridge.emitChange(message);
   }
 
 }
